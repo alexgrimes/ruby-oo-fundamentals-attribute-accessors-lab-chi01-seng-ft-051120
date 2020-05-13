3f1cc2b2ffa0a2ee6e9fd84a9ee47b1d71ccd8c7
Alex Grimes <alex@alexgrimes.com>
2020-05-12 19:47:23 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,32 @@
-## code your solution here. 
+# ## code your solution here. 
+# class Person
+ 
+#     attr_reader :name
+#     attr_writer :name
+   
+# end
+
+# class Person
+ 
+#     def name=(name)
+#       @name = name
+#     end
+   
+#     def name
+#       @name
+#     end
+# end
+
+class Cat
+    attr_accessor :name
+    def meow
+        puts "meow!"
+    end
+end
+
+maru = Cat.new
+#maru.name = "Maru"
+# jay_z.name = "Shawn Carter"
+# jay_z.name
+#   => "Shawn Carter"
+
